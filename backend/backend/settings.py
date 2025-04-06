768fee07b8bc136483257c3b2487535cff09c8dd
taysir-zt <zttayssir25@email.com>
2025-04-07 00:16:16 +0200

    API utilisateurs et installations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,12 +111,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": True,  # ✅ Activation de la blacklist
+    "BLACKLIST_AFTER_ROTATION": True, 
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
     "AUTH_HEADER_TYPES": ("Bearer",),
--- backend/installations/urls.py
+++ backend/installations/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import InstallationViewSet
+from django.urls import path
+from .views import (
+    AjouterInstallationView,
+    ModifierInstallationView,
+    SupprimerInstallationView,
+    ListerInstallationsView,
+    DetailsInstallationView,
+    StatistiquesInstallationsView
+)
 
-router = DefaultRouter()
-router.register(r'installations', InstallationViewSet)
 urlpatterns = [
-        path('api/', include(router.urls)),
+    path('installations/', ListerInstallationsView.as_view(), name='lister_installations'),
+    path('installations/<int:installation_id>/', DetailsInstallationView.as_view(), name='details_installation'),
+    path('ajouter-installation/', AjouterInstallationView.as_view(), name='ajouter-installation'),
+    path('modifier-installation/<int:installation_id>/', ModifierInstallationView.as_view(), name='modifier-installation'),  # Assurez-vous que l'ID correspond
+    path('supprimer-installation/<int:installation_id>/', SupprimerInstallationView.as_view(), name='supprimer-installation'),
+    path('installations/statistiques/', StatistiquesInstallationsView.as_view(), name='statistiques_installations'),
 ]

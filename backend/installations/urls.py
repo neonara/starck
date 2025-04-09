26a01ebc6c091e0c670fd89a14bcb6d1066d0915
unknown <you@example.com>
2025-04-10 00:43:00 +0200

    modif installation view

--- backend/installations/urls.py
+++ backend/installations/urls.py
@@ -10,10 +10,10 @@ from .views import (
 
 urlpatterns = [
     path('installations/', ListerInstallationsView.as_view(), name='lister_installations'),
-    path('installations/<int:installation_id>/', DetailsInstallationView.as_view(), name='details_installation'),
+    path('detail-installation/<int:id>/', DetailsInstallationView.as_view(), name='detail-installation'),
     path('ajouter-installation/', AjouterInstallationView.as_view(), name='ajouter-installation'),
-    path('modifier-installation/<int:installation_id>/', ModifierInstallationView.as_view(), name='modifier-installation'),  
+    path('modifier-installation/<int:id>/', ModifierInstallationView.as_view(), name='modifier-installation'),
     path('supprimer-installation/<int:installation_id>/', SupprimerInstallationView.as_view(), name='supprimer-installation'),
-    path('installations/statistiques/', StatistiquesInstallationsView.as_view(), name='statistiques_installations'),
+    path('statistiques/', StatistiquesInstallationsView.as_view(), name='statistiques_installations'),
   
 ]

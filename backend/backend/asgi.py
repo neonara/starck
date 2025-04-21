bc1dc6f753a595fa298bb0b60831a393daa369d0
unknown <you@example.com>
2025-04-21 13:17:12 +0100

    merge conflicts

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notification.routing import websocket_urlpatterns as notification_websocket_urlpatterns
-from alarme.routing import websocket_urlpatterns as alarme_websocket_urlpatterns
+
 from users.jwt_middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
@@ -10,7 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django_asgi_app = get_asgi_application()
 
 # Combinez les websocket_urlpatterns des notifications et des alarmes
-websocket_urlpatterns = notification_websocket_urlpatterns + alarme_websocket_urlpatterns
+websocket_urlpatterns = notification_websocket_urlpatterns 
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,  # Gérer les requêtes HTTP
